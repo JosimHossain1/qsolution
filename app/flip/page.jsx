c1215ecf0675cc335ac75b3bc3b473a50c24299d
Josim Hossain <josimhosssain842@gmail.com>
2023-10-04 10:07:22 +0600

    Flip Book Add

--- app/flip/page.jsx
+++ app/flip/page.jsx
@@ -3,19 +3,45 @@ import Image from "next/image";
 import React, { useState } from "react";
 import PageFlip from "react-pageflip";
 
-const totalNumberOfPages = 10; // Total number of pages in the book
-
 // Define an array of image URLs for each page
 const pageImages = [
-  "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-  "https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-  // Add URLs for the remaining pages here
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_1_bangla.f8bc3f8d.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_1_english.095ec65f.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_1_math.3f7d7fdc.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_2_bangla.72e5d555.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_2_english.4f9295ce.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_2_math.4ad29799.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_bangla.faadb0de.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_english.fc3c2c0e.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_math.e8c8323b.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_science.7e73b927.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_gbs.ad3737b6.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_islam.8b232dcb.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_hindu.8549ef47.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_buddha.11fdb842.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_3_cristian.69c14c19.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_bangla.0a2dcf82.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_english.37a7fe19.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_math.55114d8a.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_science.0896f205.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_bgs.8403dc1e.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_islam.9b347859.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_hindu.6fd81cf7.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_buddha.3d70659a.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_4_cristian.e24cb45c.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_bangla.07133b47.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_english.d4839e4f.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_math.c5536cbc.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_science.aeb99093.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_bgs.2236b047.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_islam.87db23ba.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_hindu.e47e5f3a.png&w=750&q=75",
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_buddha.5f264e57.png&w=750&q=75",
+
+  "https://qimage.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fclass_5_cristian.89fe88f8.png&w=750&q=75",
 ];
 
+const totalNumberOfPages = pageImages.length; // Total number of pages in the book
 const IndexPage = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [inputPage, setInputPage] = useState(currentPage + 1);
@@ -41,9 +67,11 @@ const IndexPage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Bangladesh Book</h1>
+      <h1 className="text-3xl font-bold mb-4 text-center text-white">
+        Book Cover Flip
+      </h1>
 
-      <div className="text-center mb-4">
+      <div className="text-center mb-4 hidden ">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded-full mr-2"
           onClick={prevPage}
@@ -72,9 +100,9 @@ const IndexPage = () => {
         </button>
       </div>
 
-      <div className="book-container">
+      <div className="book-container mx-auto">
         <PageFlip
-          className="book"
+          className="book mx-auto"
           width={400}
           height={600}
           size="fixed"
