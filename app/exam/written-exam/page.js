fb12cf3eb5bff7108cc79a6d7ecf4bb500387e3c
Josim Hossain <josimhosssain842@gmail.com>
2023-10-08 08:37:05 +0600

    Add new features

---
+++ app/exam/written-exam/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import comming from "@/assets/coming-soon.bn.png";
+const page = () => {
+  return (
+    <div className="text-center flex justify-center items-center h-screen ">
+      <Image src={comming} alt="Comming soon" />
+    </div>
+  );
+};
+
+export default page;
